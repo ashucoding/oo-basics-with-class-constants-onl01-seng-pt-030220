33c1d66a8479f04c03959a168fefd0c57514a385
Shana Wiltshire <shanawiltshire@gmail.com>
2020-04-07 15:52:56 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,9 +2,9 @@
 
 class Book
   attr_accessor :author, :page_count, 
-  attr_reader :title, :genre
+  attr_reader :title, :genre 
 
-  GENRES = {}
+  GENRES = []
   
   def initialize(title)
     @title = title
